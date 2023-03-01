98b3d7f4a5e6732aefc70cc6e32eff18ce62db28
AMRIT PANDEY <kabirjungpandey@gmail.com>
2023-03-01 17:28:04 +0545

    change some styles

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,12 @@
 import styles from '<import>/styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Header() {
     return (
         <header className={styles.nav}>
                 <div className={styles.logo}>Amrit.</div>
                 <ul>
-                <li>01. <a href="#About">About</a> </li>
+                <li>01. <Link href="#About">About</Link> </li>
                 <li>02. <a href="#Experience">Experience</a> </li>
                 <li>03. <a href="#Work">Work</a> </li>
                 <li>04. <a href="#Contact">Contact</a> </li>
--- components/Section.js
+++ components/Section.js
@@ -1,5 +1,6 @@
 import styles from '<import>/styles/Home.module.css'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Section() {
   return (
@@ -10,7 +11,8 @@ export default function Section() {
       <p className={styles.p4element}>I’m a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on  building accessible, human-centered products at my <a href="https://github.com/amripandey" target="_blank">Github</a>.</p>
       <div className={styles.ResumeBox}>Resume</div>
       <div className={styles.About}>
-        <h2>01.<p className={styles.p5element}> About Me----------------------</p></h2>
+
+        <Link href="#About"></Link><h2>01.<p className={styles.p5element}> About Me----------------------</p></h2>
         <p className={styles.p6element}>Hello! My name is Amrit pandey and I enjoy creating things that live on the internet. My interest in web development started back in 2012 when I decided to try editing custom portfolio themes — turns grtting know about web and taught me a lot about HTML & CSS!
           Fast-forward to today, and I’ve had the privilege of working on different types  of open-source projects, and a student-led design studio. My main focus these days is building accessible, inclusive products and digital experiences for a variety of projects.</p>
       </div>
